19ddd344575841296a851054a38bcf1a4f10c7ba
Vadzim_Kavalkou <vadzim_kavalkou@epam.com>
2022-02-20 04:40:30 +0300

    Minor fixes
    TODO:
    1. Comparators now able to return multiple identical results
    2. Amount now supports BigDecimal values
    3. Display data on load on UI
    4. Add error displaying on UI
    5. Java Docs added
    6. Unit tests added

---
+++ src/main/java/by/vk/betting/api/configuration/client/CorsingFilter.java
@@ -0,0 +1,24 @@
+package by.vk.betting.api.configuration.client;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Component
+public class CorsingFilter {
+  @Bean
+  public CorsFilter corsFilter() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    final CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(List.of("http://localhost", "http://35.240.126.169/"));
+    config.setAllowedHeaders(List.of("Origin", "Content-Type", "Accept"));
+    config.setAllowedMethods(List.of("GET", "OPTIONS"));
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+}
